ebde4808c4373817061b29c58fd11cb58bd3483f
Beatriz Soares Ferreira <beatriz_nljd@hotmail.com>
2021-06-10 17:25:25 -0300

    removi um comentario

--- rpg.cpp
+++ rpg.cpp
@@ -8,7 +8,6 @@
  * escolhas da/do jogadora/or pode mudar o final
  */
 
-// TODO Fazer uma forma do jogador ou jogadora escolher sua raça
 // TODO Sendo que cada raça tem a possibilidade de  ter mais ou menos atributos que as outras
 
 struct Personagem{
