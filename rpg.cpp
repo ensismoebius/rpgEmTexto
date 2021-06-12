6ee94d854ca45c0b8f5f8eb3915654ecbe31ae50
anaandrade216 <85773391+anaandrade216@users.noreply.github.com>
2021-06-11 23:01:42 -0300

    Update rpg.cpp

--- rpg.cpp
+++ rpg.cpp
@@ -8,9 +8,7 @@
  * escolhas da/do jogadora/or pode mudar o final
  */
 
-// TODO Fazer uma forma do jogador ou jogadora escolher sua raça
-// TODO Sendo que cada raça tem a possibilidade de  ter mais ou menos atributos que as outras
-
+..
 struct Personagem{
     std::string nome;
 
